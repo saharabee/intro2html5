dd008121566e033d750060ca74899f725f01deff
Sahara Berhe <sberhe00@lab.cpcc.edu>
2017-02-06 21:01:06 -0500

    Chnage made on 2/6/17

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ var app = express();
 //Configure express to deliver static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, resp){
-    res.sendFile('index.html');
+app.get('/', function(req, res){
+    //res.sendFile('index.html');
+    res.sendfile(__dirname + 'public/index/html');
 });
+
+
+
+
 
 module.exports = app;
